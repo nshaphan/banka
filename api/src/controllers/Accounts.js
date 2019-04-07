1e353422d6d589e9fde9d1151ab91b81c58dc4aa
shaphan <chaphannzabonimana@gmail.com>
2019-04-07 14:19:05 +0200

    [feature] account create, toggle status, delete account

--- api/src/controllers/Accounts.js
+++ api/src/controllers/Accounts.js
@@ -2,6 +2,8 @@ import banka from '../db/db';
 import accountHelper from "../helpers/AccountHelper";
 
 class AccountsController {
+
+    // create new bank account
     accountCreate(req, res) {
 
         var account = req.body;
@@ -16,6 +18,7 @@ class AccountsController {
         account.createdOn = new Date();
         account.status = 'active';
 
+        // find user using access_token
         const accountOwner = users.find((user) => user.token === req.query.token);
 
         account.owner = accountOwner.id;
@@ -24,7 +27,7 @@ class AccountsController {
         let response = {
             status: 200,
             data: {
-                accountNumber: account.account,
+                accountNumber: account.accountNumber,
                 firstname: accountOwner.firstname,
                 lastname: accountOwner.lastname,
                 email: accountOwner.email,
@@ -36,7 +39,56 @@ class AccountsController {
 
         res.status(200).json(response);
     
-    } 
+    }
+
+    // change account status from active to dormant and vice-versa
+    toggleStatus(req, res) {
+        
+        // getting account number from url
+        let accountNumber = req.params.accountNumber;
+        let accounts = banka.accounts;
+
+        // find account index using account number
+        const accountIndex = accounts.findIndex((account) => account.accountNumber === accountNumber);
+        
+        // updating account based on account index
+        if(banka.accounts[accountIndex].status == 'active') {
+            banka.accounts[accountIndex].status = 'dormant';
+        } else {
+            banka.accounts[accountIndex].status = 'active';
+        }  
+
+        // response object
+        let response = {
+            status: 200,
+            data: {
+                accountNumber: banka.accounts[accountIndex].accountNumber,
+                status: banka.accounts[accountIndex].status
+            }
+        }
+
+        res.status(200).json(response);
+    }
+
+    deleteAccount(req, res) {
+        // getting account number from url
+        let accountNumber = req.params.accountNumber;
+        let accounts = banka.accounts;
+
+        // find account index using account number
+        const accountIndex = accounts.findIndex((account) => account.accountNumber === accountNumber);
+        
+        // updating deletedAt Account property
+        banka.accounts[accountIndex].deletedAt = new Date();
+
+        let response = {
+            status: 200,
+            message: "Account successfully deleted"
+        }
+
+        res.json(response);
+    }
+    
 }
 
 const accountsController = new AccountsController();
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -11,6 +11,9 @@ const base_url = '/api/'+ api_version;
 router.get(base_url +'/users', usersController.getUsers);
 router.post(base_url +'/auth/signup', usersController.signup);
 router.post(base_url +'/auth/signin', usersController.signin);
+
 router.post(base_url +"/accounts", accountsController.accountCreate);
+router.patch(base_url +'/account/:accountNumber', accountsController.toggleStatus);
+router.delete(base_url +'/accounts/:accountNumber', accountsController.deleteAccount);
 
 export default router;
