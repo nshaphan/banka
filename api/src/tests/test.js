26c8daa1cc13742f277cd3d7af439dd16e6c4073
shaphan <chaphannzabonimana@gmail.com>
2019-04-09 09:29:54 +0200

    finished test case for basic endpoints

--- api/src/tests/test.js
+++ api/src/tests/test.js
@@ -9,19 +9,15 @@ use(chaiHttp);
 const api_version = 'v1';
 const base_url = '/api/'+ api_version; 
 
-var testUser = bankaTest.users[0];
-var testAccount = bankaTest.accounts[0];
-
-
-
-
-
 describe('POST /auth/signup', () => {
+
+    var testUser = bankaTest.users[0];
     it('should be able to create a new user', (done) => {
         request(app)
             .post(base_url +'/auth/signup')
             .send(testUser)
             .end((err, res) => {
+                // Expect status to Ok!
                 expect(res.status).to.eql(200);
                 done(err);
             });
@@ -29,21 +25,24 @@ describe('POST /auth/signup', () => {
 });
 
 describe("POST /auth/signin", () => {
-        
-    it("should be able to authenticate the user", (done) => {
+    
+    var testUser = bankaTest.users[0];
+    it('should be able to create a new user', (done) => {
         request(app)
             .post(base_url +'/auth/signin')
             .send({email: testUser.email, password: testUser.password})
             .end((err, res) => {
+
+                // Expect status to be equal to OK!
                 expect(res.status).to.eql(200);
                 done(err);
             });
-            
     });
 });
 
 describe("POST /accounts", () => {
 
+    var testAccount = bankaTest.accounts[0];
     it("Should be able to create a new account", (done) => {
         request(app)
             .post(base_url +'/accounts?token=45erkjherht45495783')
@@ -54,4 +53,62 @@ describe("POST /accounts", () => {
             });
     });
 });
+
+describe("PATCH /account/<account-number>", () => {
+
+    it("Should be able to activate or disactivate account", (done) => {
+        request(app)
+            .patch(base_url +'/account/20183444095')
+            .end((err, res) => {
+                expect(res.status).to.eql(200);
+                done(err);
+            });
+    });
+});
+
+
+describe("DELETE /accounts/<account-number>", () => {
+
+    it("Should be able to delete account", (done) => {
+        request(app)
+            .delete(base_url +'/accounts/20183444095')
+            .end((err, res) => {
+                expect(res.status).to.eql(200);
+                done(err);
+            });
+    });
+});
+
+describe("POST /transactions/<account-number>/debit", () => {
+
+    var testTransact = bankaTest.transactions[0];
+    it("Should be able to debit account", (done) => {
+        request(app)
+            .post(base_url +'/transactions/20183444095/debit?token=45erkjherht45495783')
+            .send(testTransact)
+            .end((err, res) => {
+                expect(res.status).to.eql(200);
+                done(err);
+            });
+    });
+});
+
+describe("POST /transactions/<account-number>/credit", () => {
+
+    var testTransact = bankaTest.transactions[0];
+    it("Should be able to credit account", (done) => {
+        request(app)
+            .post(base_url +'/transactions/20183444095/credit?token=45erkjherht45495783')
+            .send(testTransact)
+            .end((err, res) => {
+                expect(res.status).to.eql(200);
+                done(err);
+            });
+    });
+});
+
+
+
+
+
 
