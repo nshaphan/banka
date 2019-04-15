efbbcc0b5ceebe078770c69e058f02409d070a2e
shaphan <chaphannzabonimana@gmail.com>
2019-04-15 10:56:37 +0200

    validating user input

--- api/src/controllers/Users.js
+++ api/src/controllers/Users.js
@@ -9,10 +9,16 @@ class UsersController {
 
     signup(req, res) {
 
-        var user = req.body;
+        var {email, firstname, lastname, password, type, isadmin} = req.body;
         
         // Increment user id for new user
         user.id = banka.users.length + 1;
+        user.email = email;
+        user.firstname = firstname;
+        user.lastname = lastname;
+        user.password = password,
+        user.type = type;
+        user.isadmin = isadmin;
         user.token = shortid.generate();
 
         // Adding new user to database
@@ -34,11 +40,21 @@ class UsersController {
     }
 
     signin(req, res) {
-        var credentials = req.body;
+        var { email, password } = req.body;
 
         // find user with provided credentials 
-        var user = banka.users.find((user) => user.email == credentials.email 
-                                                && user.password == credentials.password);
+        var user = banka.users.find((user) => user.email == email 
+                                                && user.password == password);
+        
+        var userIndex = banka.users.findIndex((user) => user.email == email && user.password == password);
+
+        // nbanka.users[userIndex].token
+        if(!user) {
+            res.status(400).json({
+                status: 400,
+                error: "invalid username or password"
+            });
+        }
         // sign in response specifications
         var response = {
             status : 200,
