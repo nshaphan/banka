3ed3d690f4c4bde302aee418e4e70d6abba514ac
shaphan <chaphannzabonimana@gmail.com>
2019-05-05 17:51:05 +0200

    [feature] function to get all transactions from API

--- UI/assets/js/transactions.js
+++ UI/assets/js/transactions.js
@@ -2,3 +2,8 @@ let getAccountTransactions = (accountNumber, data = null) => {
 	let accessToken = getAccessToken();
     return getRequest(baseUrl +'/accounts/'+ accountNumber +'/transactions', null, accessToken);
 }
+
+let getTransactions = (data = null) => {
+	let accessToken = getAccessToken();
+	return getRequest(baseUrl +'/transactions', null, accessToken);
+}
