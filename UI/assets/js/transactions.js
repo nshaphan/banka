28d89fb4610db1125a029fafc37a71953d8c7125
shaphan <chaphannzabonimana@gmail.com>
2019-05-05 19:06:34 +0200

    [feature] adding new transaction

--- UI/assets/js/transactions.js
+++ UI/assets/js/transactions.js
@@ -1,9 +1,14 @@
-let getAccountTransactions = (accountNumber, data = null) => {
+const getAccountTransactions = (accountNumber, data = null) => {
 	let accessToken = getAccessToken();
     return getRequest(baseUrl +'/accounts/'+ accountNumber +'/transactions', null, accessToken);
 }
 
-let getTransactions = (data = null) => {
+const getTransactions = (data = null) => {
 	let accessToken = getAccessToken();
 	return getRequest(baseUrl +'/transactions', null, accessToken);
+}
+
+const newTransaction = (transaction, type, accountNumber) => {
+	let accessToken = getAccessToken();
+	return postRequest(baseUrl +'/transactions/'+ accountNumber +'/'+ type, transaction, accessToken);
 }
